aaa05a0d085df60f148949098d07ead5cad2e61a
jirateep <charoenshop@gmail.com>
2024-02-13 19:19:10 +0700

    update date

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 14, 2024 00:00:00").getTime();
+var countDownDate = new Date("Feb 13, 2024 21:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
